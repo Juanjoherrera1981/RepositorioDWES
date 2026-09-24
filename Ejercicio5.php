d7bbbaf85504163dd1b779fdcfc6c1107466da2f
Juanjo <juanjoherrera1981@gmail.com>
2026-09-24 17:38:52 +0200

    Mi primer commit

---
+++ Ejercicio5.php
@@ -0,0 +1,12 @@
+<?php
+
+$mensaje = "Hola";
+echo $mensaje ."</br>";
+$mensaje = $mensaje  ." Mundo";
+echo $mensaje ."</br>";
+$mensaje = $mensaje  ." Maravilloso";
+echo $mensaje ."</br>";
+
+
+
+?>
