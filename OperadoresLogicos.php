9a181543d4ba5af687605fbfa7d5ede469bc1a40
Juanjo <juanjoherrera1981@gmail.com>
2026-09-25 15:37:41 +0200

    Ejercicio operadores logicos

---
+++ OperadoresLogicos.php
@@ -0,0 +1,16 @@
+<?php
+$logueado=true;
+$administrador=true;
+$voluntario=false;
+
+echo "Estas logueado y eres administrador";
+var_dump($logueado&&$administrador);
+echo "Eres administrador o voluntario?";
+var_dump($administrador||$voluntario);
+echo "Estas logueado?";
+var_dump(!$logueado);
+echo "Eres aministrador o voluntario?";
+var_dump($administrador xor $voluntario) ;
+
+
+?>
